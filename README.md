299e900978f0fb7ab00040ba3ecb838f0bc66b5b
Kaique646 <kaiqueteixeira646@gmail.com>
2022-07-12 14:53:15 -0300

    dungeons-shop update

--- README.md
+++
@@ -1,2 +0,0 @@
-# DugeonsShop
-Projeto De Loja Virtual feita em JAVA para estudo de Interface Gráfica com conexão de Banco de Dados.
---
+++ src/dungeons/shop/panels/ItemPanel.java
@@ -0,0 +1,134 @@
+package dungeons.shop.panels;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+import dungeons.shop.bd.CallDatabase;
+import javafx.scene.image.Image;
+
+public class ItemPanel extends JPanel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JPanel pnEme = new JPanel();
+	JPanel pnImg = new JPanel();
+	JPanel pnall;
+	JLayeredPane lpn = new JLayeredPane();
+	
+	JLabel emerald = new JLabel(new ImageIcon(getClass().getResource("Emerald17X24.png")));
+	ImageIcon frameIcon = new ImageIcon(getClass().getResource("Frame.png"));
+	JLabel frame = new JLabel(frameIcon);
+	JLabel item, nome ,preco;
+	
+	
+	JCheckBox ck = new JCheckBox();
+
+	ImageIcon btoff = new ImageIcon(getClass().getResource("buyButton20X21.png"));
+	ImageIcon bton = new ImageIcon(getClass().getResource("buyButtonON20X21.png"));
+	
+	ArrayList<Integer> selecionados = new ArrayList<>();
+	
+	
+	public ItemPanel(int index){
+
+		if(CallDatabase.listacomuns.get(index-1).contentEquals("Y")) {
+			setBorder(BorderFactory.createLineBorder(new Color(156, 156, 156)));
+		}
+		if(CallDatabase.listararos.get(index-1).contentEquals("Y")) {
+			setBorder(BorderFactory.createLineBorder(new Color(26, 199, 49)));
+		}
+		if(CallDatabase.listaunicos.get(index-1).contentEquals("Y")){
+			setBorder(BorderFactory.createLineBorder(new Color(247, 121, 25)));
+		}
+
+		
+		
+		init();
+		def(index-1);
+		try {
+			item = new JLabel(new ImageIcon(getClass().getResource(CallDatabase.nomes.get(index-1)+".png")));
+			pnImg.add(item);
+		} catch (Exception e) {
+			
+		}
+		
+		preco = new JLabel(String.valueOf(CallDatabase.precos.get(index-1))+" R$");
+		nome = new JLabel(CallDatabase.nomes.get(index-1),SwingConstants.CENTER);
+		pnEme.add(emerald);
+		pnEme.add(preco);
+		pnImg.setLayout(new GridLayout(0,1));
+	
+		preco.setForeground(Color.white);
+		nome.setForeground(Color.white);
+		preco.setFont(new Font("Courier new", 0,12));
+		nome.setFont(new Font("Courier new", 0,12));
+		pnEme.setBackground(null);
+		pnImg.setBackground(null);
+		
+		
+		ck.setSize(120,180);
+		ck.setOpaque(false);
+		ck.setIcon(btoff);
+		
+		
+		pnall = new JPanel(new BorderLayout());
+		pnall.setSize(111,180);
+		pnall.setBackground(new Color(0,0,0,0));
+		
+		pnall.add(pnEme, "North");
+		pnall.add(pnImg, "Center");
+		pnall.add(nome, "South");
+		pnall.setOpaque(false);
+		
+		lpn.add(pnall);
+		lpn.add(ck);
+		lpn.setPreferredSize(new Dimension(111,180));
+		lpn.setOpaque(false);
+		add(lpn);
+
+		
+		
+	}
+
+	public void def(int index) {
+		ck.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent arg0) {
+				if(ck.isSelected()) {
+					ck.setIcon(bton);
+					CallDatabase.selected.add(index-1);
+					
+				}else {
+					ck.setIcon(btoff);
+					for (int i = 0; i < CallDatabase.selected.size(); i++) {
+						if(CallDatabase.selected.get(i)==index-1) {
+							CallDatabase.selected.remove(i);
+						}
+					}
+				}
+			}
+			
+		});
+	}
+
+	public void init() {
+		//setOpaque(false);
+		setSize(111,180);
+		setLayout(new BorderLayout(0,0));
+		
+		setBackground(new Color(13,8,13));
+	}
+
+}
